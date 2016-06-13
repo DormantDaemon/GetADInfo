9b0420461672eded40623c0cf9190107e87e4446
clarcom87 <clarcom87@gmail.com>
2016-06-13 16:56:51 -0500

    second commit

---
+++ Get-InActiveUsers.ps1
@@ -0,0 +1,7 @@
+﻿Import-module activedirectory
+$domain = (Get-ADDomain | Select Name).name
+$DaysInactive = 30
+$time = (Get-Date).Adddays(-($DaysInactive))
+$SearchBase = $null
+
+Get-ADUser -Filter {passwordlastset -lt $time -and whencreated -lt $time -and enabled -eq $false} -Properties description,passwordlastset,passwordexpired,whencreated,whenchanged  | select-object samaccountname,description,passwordlastset,passwordexpired,whencreated,whenchanged | export-csv $PSScriptRoot\DisabledUserAccounts.csv –NoTypeInformation -Force
